55cf6dc10367d86198bc6feee4ededbf7c005313
Alejandro Jiménez <aljrico@gmail.com>
2018-04-08 16:01:47 +0200

    redesign of scrape method

--- city-pop.R
+++ city-pop.R
@@ -12,42 +12,36 @@ library(rvest)
 
 
 
-# URL ---------------------------------------------------------------------
+# Most Populated Cities ---------------------------------------------------------------------
 
+# URL
 path<-"https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population"
 webpage <- getURL(path)
-webpage <- readLines(tc <- textConnection(webpage)) 
-close(tc)
+url <- webpage
 
-# Fetch all web content
-pagetree <- htmlTreeParse(webpage, error=function(...){}, useInternalNodes = TRUE, encoding = "UTF-8")
+# Fetch HTML
+mytable <- read_html(url) %>% 
+	html_nodes("table")
 
-# Extract table header and contents
-tablehead_us <- xpathSApply(pagetree, "//*/table/tr", xmlValue)
+# Extract table of interest
+most_pop_cities <- mytable[[4]] %>% html_table(fill = TRUE, header = NA)
 
+# Cleaning
+most_pop_cities <- most_pop_cities[,c(4,2)]
+colnames(most_pop_cities) <- c("pop", "city")
+most_pop_cities$city <- gsub("\\[(.*?)\\]", "", most_pop_cities$city)
+most_pop_cities$pop <- as.numeric(gsub(",", "", most_pop_cities$pop))
 
-city <- c()
-pop <- c()
-hist_pop <- list()
-for(i in 17:323){
-	# Select a row from the table
-	row <- tablehead_us[i]
-	row <- strsplit(row,"\n")
 	
-	# Extract city name and population
-	city[i - 16] <- gsub("\\[(.*?)\\]", "", row[[1]][[2]])
-	pop[i - 16] <- as.numeric(gsub(",", "",row[[1]][[4]]))
-	
-	
-	
-	# Historical of each City ----------------------------------------------------
-	
-	cityname <- gsub(" ", "_",city[i-16])
+# Historical of each City ----------------------------------------------------
+
+cityname <- gsub(" ", "_",most_pop_cities$city)
+
+function(cityname){
 	if(cityname == "New_York") {cityname <- paste0(cityname, "_City")}
 	if(cityname == "Phoenix"){cityname <- paste0("Phoenix,_Arizona")}
 	path <- paste0("https://en.wikipedia.org/wiki/",cityname)
-
-	url <- path
+		url <- path
 	mytable <- read_html(url) %>% 
 		html_nodes("table")
 		
@@ -59,9 +53,6 @@ for(i in 17:323){
 	df <- df[-1, c(1,2)]
 	df[,2] <- as.numeric(gsub(",", "",df[,2]))
 	df <- df[complete.cases(df),]
-
-	hist_pop[[cityname]] <- df
-	rm(df)
-}	
-
-most_pop_cities <- data.frame(city = city, pop = pop)
+		hist_pop[[cityname]] <- df
+	return(hist_pop)
+}
